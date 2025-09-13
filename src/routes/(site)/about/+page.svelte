05f845f8fa5f9b1b66275f470d0409d031f47981
Moye Odiase <moyeodiase@gmail.com>
2025-09-13 12:54:43 +0100

    Update about me section

--- src/routes/(site)/about/+page.svelte
+++ src/routes/(site)/about/+page.svelte
@@ -1,12 +1,35 @@
-<div class="min-h-full gap-8 flex flex-col">
-  <section class="flex flex-col gap-2 text-justify max-w-[800px]">
-    <p>I am currently based in <strong>London, UK</strong>. I am a software
-    engineer with a keen interest in using technology to solve problems and
-    improve lives.</p> <p>I am a keen naturalist and environmentalist and have
-    spent the last few years working in cleantech and believe that an
-    <strong>abundant but sustainable future is possible.</strong>. I have
-    recently refocused my time on learning more about AI and how it can be used
-    to usher in a <strong>new era of human and environmental
-    flourishing.</strong></p>
+<div class="min-h-full flex flex-col gap-8">
+  <section class="section">
+    <p>
+      I am currently based in <strong>London, UK</strong>. I am a software engineer with a keen
+      interest in using technology to solve problems and improve lives.
+    </p>
+    <p>
+      I am a keen naturalist and environmentalist and have spent the last few years working in
+      cleantech and believe that an
+      <strong>abundant but sustainable future is possible.</strong>. I have recently refocused my
+      time on learning more about AI and how it can be used to usher in a
+      <strong>new era of human and environmental flourishing.</strong>
+    </p>
+  </section>
+  <section class="section">
+    <h2 class="text-lg font-bold mb-2">Education</h2>
+    <p>
+      I graduated from the University of Cambridge in 2018 having studied for an MSc. in Natural
+      Sciences, during which I studied Mathematics, Chemistry, Physics and Computer Science and
+      ultimately specialised in theoretical chemistry.
+    </p>
   </section>
 </div>
+
+<style>
+  .section {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+    text-align: justify;
+    max-width: 800px;
+    flex-grow: 0;
+  }
+</style>
