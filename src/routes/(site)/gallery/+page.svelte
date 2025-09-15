c2617d5b1a2c1d64f9d9caaa4fc0ab11691655ea
Moye <moyeodiase@hotmail.co.uk>
2025-09-15 18:20:15 +0100

    Update gallery styling

--- src/routes/(site)/gallery/+page.svelte
+++ src/routes/(site)/gallery/+page.svelte
@@ -1,6 +1,6 @@
-<div class="flex items-center justify-center min-h-full text-center">
+<div class="flex items-center justify-center min-h-full text-center bg-[var(--nord4)]">
   <div>
-    <h1 class="text-4xl font-bold mb-4">Gallery</h1>
-    <p class="text-lg text-secondary">Coming soon...</p>
+    <h1 class="text-4xl font-bold mb-4 text-contrast">Gallery</h1>
+    <p class="text-lg text-contrast">Coming soon...</p>
   </div>
 </div>
