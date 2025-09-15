93906a878d11d80fec44f3003260bbd3e6a48ae7
Moye <moyeodiase@hotmail.co.uk>
2025-09-15 17:24:10 +0100

    Change hero title

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,7 +5,7 @@
 <div class="flex flex-col items-center pt-[40px]">
   <h1 class="text-4xl text-center mb-4 font-medium">Moyewa Odiase</h1>
   <div class="flex flex-col items-center gap-1 mb-8">
-    <p class="text-center font-semibold">Full Stack Software Engineer</p>
+    <p class="text-center font-semibold">Naturalist, Technologist, Software Engineer</p>
     <p class="text-center font-medium">Interested in cleantech, and safe and fair AI</p>
   </div>
   <div class="w-96 h-96 bg-gray-500 rounded-full flex items-center justify-center overflow-hidden">
