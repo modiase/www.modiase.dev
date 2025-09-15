5464236b5eb168014e4b8c807298a667d116a9c8
Moye <moyeodiase@hotmail.co.uk>
2025-09-15 20:36:48 +0100

    Update svelte-kit and svelte-check versions

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [sveltekit()],
   resolve: {
     alias: {
-      $styles: path.resolve('./src/styles'),
+      $styles: resolve('./src/styles'),
     },
   },
   css: {
