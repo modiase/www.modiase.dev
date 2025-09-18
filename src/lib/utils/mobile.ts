236d14d110c143141fd0e4ba7a78648dc75719c8
Moye <moyeodiase@hotmail.co.uk>
2025-09-18 10:56:55 +0100

    Fix gallery layout
    The sidebar was not accounted for on non-mobile layout.

--- src/lib/utils/mobile.ts
+++ src/lib/utils/mobile.ts
@@ -2,10 +2,11 @@ import { onMount } from 'svelte';
 import { writable } from 'svelte/store';
 
 export const isMobile = writable(false);
+export const MobileWidthPx = 500;
 
 export function initMobileDetection() {
   onMount(() => {
-    const checkMobile = () => isMobile.set(window.innerWidth < 500);
+    const checkMobile = () => isMobile.set(window.innerWidth < MobileWidthPx);
 
     checkMobile();
     window.addEventListener('resize', checkMobile);
