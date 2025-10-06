bed57c8f68b2de24fd93c64bdfc90b8a88d0cb78
Moye Odiase <moyeodiase@gmail.com>
2025-10-06 10:22:51 +0100

    Fix verbiage and gallery background

--- src/routes/(site)/gallery/+page.svelte
+++ src/routes/(site)/gallery/+page.svelte
@@ -6,9 +6,10 @@
   <meta name="description" content="Photos" />
 </svelte:head>
 
-<div class="flex items-center justify-center min-h-full text-center bg-contrast">
+<div class="flex items-center justify-center min-h-full text-center bg-surface">
   <div>
     <h1 class="text-4xl font-bold mb-4 text-text-primary">Gallery</h1>
-    <p class="text-lg text-text-primary">A photo tells a thousand words. Coming soon...</p>
+    <p class="text-lg text-text-primary">A photo is worth a thousand words.</p>
+    <p class="text-lg text-text-primary">Coming soon...</p>
   </div>
 </div>
