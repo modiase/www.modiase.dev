a4e3b6d47983c53f7461321b5a8c40d5af041ff0
Moye Odiase <moyeodiase@gmail.com>
2025-11-25 17:29:19 +0000

    Update image and tweak image styling
    - Make buttons below hero smaller.
    - Make hero slightly smaller to align with text.
    - Use new photo.

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -7,10 +7,9 @@
   <h1 class="text-4xl text-center mb-4 font-medium">Moyewa Odiase</h1>
   <div class="flex flex-col items-center gap-1 mb-8">
     <p class="text-center font-semibold">Naturalist, Technologist and Software Engineer</p>
-    <p class="text-center">Cleantech and safe AI</p>
   </div>
   <div
-    class="w-64 h-64 sm:w-80 sm:h-80 lg:w-96 lg:h-96 bg-gray-500 rounded-full flex items-center justify-center overflow-hidden"
+    class="w-48 h-48 sm:w-30 sm:h-30 lg:w-64 lg:h-64 bg-gray-500 rounded-full flex items-center justify-center overflow-hidden"
   >
     <img
       src="/assets/images/hero.jpg"
@@ -22,27 +21,27 @@
   <div class="flex justify-center space-x-8 mt-8">
     <Tooltip text="Posts">
       <Link href="/posts" secondary>
-        <i class="fas fa-comment text-3xl"></i>
+        <i class="fas fa-comment text-2xl"></i>
       </Link>
     </Tooltip>
     <Tooltip text="Gallery">
       <Link href="/gallery" secondary>
-        <i class="fas fa-camera text-3xl"></i>
+        <i class="fas fa-camera text-2xl"></i>
       </Link>
     </Tooltip>
     <Tooltip text="GitHub">
       <Link href="https://github.com/modiase" secondary>
-        <i class="fab fa-github text-3xl"></i>
+        <i class="fab fa-github text-2xl"></i>
       </Link>
     </Tooltip>
     <Tooltip text="LinkedIn">
       <Link href="https://www.linkedin.com/in/moyewa-odiase-6b4698106/" secondary>
-        <i class="fab fa-linkedin text-3xl"></i>
+        <i class="fab fa-linkedin text-2xl"></i>
       </Link>
     </Tooltip>
     <Tooltip text="Email">
       <Link href="mailto:hello@modiase.dev" secondary>
-        <i class="fas fa-envelope text-3xl"></i>
+        <i class="fas fa-envelope text-2xl"></i>
       </Link>
     </Tooltip>
   </div>
